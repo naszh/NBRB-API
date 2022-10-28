da0c694e1b8b0dd518d39ee68e2d4754d71f174b
Anastasiya Kaskevich <106557182+naszh@users.noreply.github.com>
2022-10-28 17:56:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+const baseUrl = 'https://www.nbrb.by/api/exrates/';
+
+fetch(`${baseUrl}currencies`)
+  .then(response => response.json())
+  .then(parseData)
+  .then(addToSelect);
+
+function parseData(resp) {
+  const set = new Set();
+
+  const obj = resp
+  .map(({
+    Cur_ID: id,
+    Cur_ParentID: parent,
+    Cur_Name: name,
+    Cur_DateStart: dateS,
+    Cur_DateEnd: dateE,
+  }) => {
+    return {
+      id, parent, name, dateS, dateE,
+    };
+  })
+  .reduce(
+    (acc, el) => {
+      if (!acc[el.parent] && !acc[el.name]) {
+        const arr = [el];
+        acc[el.parent] = arr;
+        acc[el.name] = arr;
+      } else if (acc[el.parent] && !acc[el.name]) {
+        acc[el.name] = acc[el.parent];
+        acc[el.parent].push(el);
+      } else if (!acc[el.parent] && acc[el.name]) {
+        acc[el.parent] = acc[el.name];
+        acc[el.parent].push(el);
+      } else {
+        acc[el.parent].push(el);
+      }
+      return acc;
+    }, {});
+
+  Object.values(obj).forEach(el => set.add(el));
+
+  return [...set].map(el => el.reduce(
+    (acc, { name, id, dateS, dateE }) => {
+      if (!acc.name) acc.name = name;
+      if (!acc.currs) acc.currs = [];
+
+      acc.currs.push({ id, dateS, dateE });
+      return acc;
+    }, {}));
+};
+
+// ====================================
+const main = document.querySelector('main');
+const select = document.querySelector('#curr');
+const fromDate = document.querySelector('#fromDate');
+const toDate = document.querySelector('#toDate');
+
+
+window.addEventListener('load', function () {
+  var d = new Date();
+  var day = d.getDate(); 
+  if (day < 10) day = '0' + day;
+
+  var month = d.getMonth() + 1; 
+  if (month < 10) month = '0' + month;
+
+  var year = d.getFullYear();
+
+  fromDate.value = `${year}-${month}-${day}`;
+  toDate.value = `${year}-${month}-${day}`;
+}, false);
+
+function addToSelect(curr) {
+  const arr = [];
+  for (let el of curr) {
+    arr.push(el.name);
+  };
+  arr.sort();
+
+  for (let name of arr) {
+    const option = document.createElement('option');
+    option.text = `${name}`;
+    option.value = `${name}`;
+    select.add(option);
+  };
+};
