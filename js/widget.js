08c200cee584a825ebfc3885b068ca03f394b391
naszh <naszh99@gmail.com>
2022-11-15 15:58:58 +0300

    Revert "update fn generateWidgetVal()"
    This reverts commit 7281bc41d0988f81872971d072d84399fed3a8c8.

--- js/widget.js
+++ js/widget.js
@@ -1,49 +1,59 @@
 function generateWidgetVal(curr) {
-	const currNames = ['US Dollar', 'EURO', 'Russian Ruble', 'Polish Zloty'];
-	const currForWidget = curr.filter(el => {
-		return currNames.includes(el.name);
-	});
-	getDataWidget(currForWidget, getToday());
+  const currForWidget = [];
+
+  curr.forEach(el => {
+    switch (el.name) {
+      case 'US Dollar':
+      case 'EURO':
+      case 'Russian Ruble':
+      case 'Polish Zloty':
+        currForWidget.push(el);
+        break;
+    }
+    return currForWidget;
+  });
+
+  getDataWidget(currForWidget, getToday());
 }
 
 function getDataWidget(curr, today) {
-	const links = [];
+  const links = [];
 
-	curr.forEach(item => {
-		item.currs.map(el => {
-			if (
-				new Date(today).getTime() <= new Date(el.dateE).getTime() &&
-				new Date(today) >= new Date(el.dateS).getTime()
-			) {
-				links.push(`${baseUrl}/rates/${el.id}?ondate=${today}`);
-			}
-		});
-	});
+  curr.forEach(item => {
+    item.currs.map(el => {
+      if (
+        new Date(today).getTime() <= new Date(el.dateE).getTime() &&
+        new Date(today) >= new Date(el.dateS).getTime()
+      ) {
+        links.push(`${baseUrl}/rates/${el.id}?ondate=${today}`);
+      }
+    });
+  });
 
-	let requests = links.map(url => fetch(url));
-	Promise.all(requests)
-		.then(responses => Promise.all(responses.map(r => r.json())))
-		.then(data => {
-			data.forEach(data => {
-				addToWidget(
-					data.Cur_OfficialRate,
-					data.Cur_Scale,
-					data.Cur_Abbreviation
-				);
-			});
-		});
+  let requests = links.map(url => fetch(url));
+  Promise.all(requests)
+    .then(responses => Promise.all(responses.map(r => r.json())))
+    .then(data => {
+      data.forEach(data => {
+        addToWidget(
+          data.Cur_OfficialRate,
+          data.Cur_Scale,
+          data.Cur_Abbreviation
+        );
+      });
+    });
 }
 
 function addToWidget(rate, scale, curr) {
-	document.querySelector(
-		`#widget-name`
-	).innerHTML = `<p>Exchange rate for today, ${getToday()}</p>`;
+  document.querySelector(
+    `#widget-name`
+  ).innerHTML = `<p>Exchange rate for today, ${getToday()}</p>`;
 
-	const currencyExch = document.createElement('div');
-	currencyExch.innerHTML = `<p>${scale}</p>
+  const currencyExch = document.createElement('div');
+  currencyExch.innerHTML = `<p>${scale}</p>
                             <p>${curr}</p>
                             <p>${rate}</p>`;
 
-	document.querySelector('.widget').append(currencyExch);
-	document.querySelector('.widget').style.backgroundColor = 'rgb(89, 147, 197)';
+  document.querySelector('.widget').append(currencyExch);
+  document.querySelector('.widget').style.backgroundColor = 'rgb(89, 147, 197)';
 }
